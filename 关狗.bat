735a9cf62af79caf179a8d56df4fce2cd2dd63c1
薛飞 <xf_lord@163.com>
2017-07-03 09:34:14 +0800

    关闭指定程序的批处理
    主要功能
    1. 自动进入Administrator模式
    2. 每隔20秒钟检测是否有指定名称的程序，有的话自动关闭。

---
+++ 关狗.bat
@@ -0,0 +1,32 @@
+@echo off
+
+
+cacls.exe "%SystemDrive%\System Volume Information" >nul 2>nul
+
+if %errorlevel%==0 goto Admin
+
+if exist "%temp%\getadmin.vbs" del /f /q "%temp%\getadmin.vbs"
+
+echo Set RequestUAC = CreateObject^("Shell.Application"^)>"%temp%\getadmin.vbs"
+echo RequestUAC.ShellExecute "%~s0","","","runas",1 >>"%temp%\getadmin.vbs"
+
+echo WScript.Quit >>"%temp%\getadmin.vbs"
+
+"%temp%\getadmin.vbs" /f
+
+if exist "%temp%\getadmin.vbs" del /f /q "%temp%\getadmin.vbs"
+
+exit
+
+
+
+:Admin
+
+
+
+@echo on
+:1
+taskkill /f /im ResetCompute.exe
+taskkill /f /im goldwind.paltform.watch.exe
+ping 127.1 -n 20 >nul 2>nul
+goto 1
